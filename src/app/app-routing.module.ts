2c8bd2cbfcd3280f3004f3620260c8b2453ef76b
PEDRO <130096211+PedroP57xd@users.noreply.github.com>
2023-12-07 19:26:39 -0500

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'ejercicio1',
+    loadChildren: () => import('./ejercicio1/ejercicio1.module').then( m => m.Ejercicio1PageModule)
+  },
+  {
+    path: 'ejercicio2',
+    loadChildren: () => import('./ejercicio2/ejercicio2.module').then( m => m.Ejercicio2PageModule)
+  },
+  {
+    path: 'ejercicio3',
+    loadChildren: () => import('./ejercicio3/ejercicio3.module').then( m => m.Ejercicio3PageModule)
+  },
+  {
+    path: 'ejercicio4',
+    loadChildren: () => import('./ejercicio4/ejercicio4.module').then( m => m.Ejercicio4PageModule)
+  },
+  {
+    path: 'ejercicio5',
+    loadChildren: () => import('./ejercicio5/ejercicio5.module').then( m => m.Ejercicio5PageModule)
+  },
 ];
 
 @NgModule({
